0419e9d29c6834f054b86ce69745b07024b21fa9
noviairawati <noviairawatinababan@gmail.com>
2023-11-10 23:31:54 +0700

    assigment week4

--- WEEK-4/README.md
+++ WEEK-4/README.md
@@ -1,4 +1,42 @@
 This is Advance Assigment, I make Coffee Store with HTML, CSS and Javascript.
 I m Applying CSS Grid, Semantic Element, Responsive Website
 
-Here's my deploy: [Coffee Store](https://magenta-douhua-62f7a3.netlify.app)
+Here's my deploy: 
+Here's my website: noviairawati.site
+
+#Deployment Prosess
+
+Netlify Sign Up Process & Connect Netlify to Github
+
+Auto Deployement on Github with Netlify
+Step 1: Add Your New Site
+
+Step 2: Link to Your GitHub (or supported version-control tool of choice)
+Clicking "Add A New Project" brings you to this screen:
+
+
+
+Be sure to push your repo to GitHub, so that all we'll need to do is link Netlify to GitHub. Click the GitHub button as illustrated in the screenshot above.
+
+Step 3: Authorize Netlify
+It's time to allow Netlify and GitHub to talk to each other. Clicking the Authorize Application button will do just that. Like it says in the image below, Netlify doesn't store your GitHub access token on our servers. If you'd like to know more about the permissions Netlify requests and why we need them, you can visit our documentation on Git provider permissions.
+
+Step 4: Select Your Repo
+
+Now that you've connected Netlify and GitHub, you can see a list of your Git repos. Choose the repo you’d like to deploy from the list.
+
+Step 5: Configure Your Settings
+
+Here you can configure your options. Make sure your publish directory is where your public site files are placed after building the site (e.g. dist) and your build command is whatever you run to build the site (e.g. npm run build). Then click the Deploy site button to continue.
+
+Step 6: Build Your Site
+Now it's time to sit back and relax. You did your part; let Netlify take care of the rest. It'll only take a minute.
+
+Step 7: All Done
+Once the build completes, your site is live! Head to the overview and you can see the URL of your newly published site.
+
+Netlify automatically generated a name for your site. Let's update that by visiting the settings tab and clicking "Change site name":
+
+
+How to connect custom domain and DNS
+
--- WEEK-4/script.js
+++ WEEK-4/script.js
@@ -1,51 +1,49 @@
 let navbar = document.querySelector('.navbar');
 
 document.querySelector('#menu-btn').onclick = () => {
-    navbar.classList.toggle('active');
-    searchForm.classList.remove('active');
-    cartItem.classList.remove('active');
+  navbar.classList.toggle('active');
+  searchForm.classList.remove('active');
+  cartItem.classList.remove('active');
 }
 
 let searchForm = document.querySelector('.search-form');
 
 document.querySelector('#search-btn').onclick = () => {
-    searchForm.classList.toggle('active');
-    navbar.classList.remove('active');
-    cartItem.classList.remove('active');
+  searchForm.classList.toggle('active');
+  navbar.classList.remove('active');
+  cartItem.classList.remove('active');
 }
 
 
 let cartItem = document.querySelector('.cart-items-container');
 
 document.querySelector('#cart-btn').onclick = () => {
-    cartItem.classList.toggle('active');
-    navbar.classList.remove('active');
-    searchForm.classList.remove('active');
+  cartItem.classList.toggle('active');
+  navbar.classList.remove('active');
+  searchForm.classList.remove('active');
 
 }
 
 window.onscroll = () => {
-    navbar.classList.remove('active');
-    searchForm.classList.remove('active')
-    cartItem.classList.remove('active')
+  navbar.classList.remove('active');
+  searchForm.classList.remove('active')
+  cartItem.classList.remove('active')
 }
 
 /*Dark/Light Mode - local storage*/
-if (localStorage.getItem('theme') == 'dark')
-    setDarkMode()
-
-function myFunction() {
-    let emoticon = '<i class="fa-sharp fa-solid fa-mug-hot"></i>'
-    let isDark = document.footer.classList.toggle('darkmode')
-
-    if (isDark) {
-        emoticon = '<i class="fa-sharp fa-solid fa-mug-hot"></i>'
-        localStorage.setItem('theme', 'dark');
-    } else {
-        emoticon = '<i class="fa-sharp fa-light fa-mug-hot"></i>'
-        localStorage.removeItem('theme')
-    }
-    document.getElementById('switch').innerHTML = emoticon
+const body = document.querySelector("body"),
+  toggle = document.querySelector(".toggle");
+let getMode = localStorage.getItem("mode");
+if (getMode && getMode === "dark") {
+  body.classList.add("dark");
+  toggle.classList.add("active");
 }
-
+toggle.addEventListener("click", () => {
+  body.classList.toggle("dark");
+  if (!body.classList.contains("dark")) {
+    return localStorage.setItem("mode", "light");
+  }
+  localStorage.setItem("mode", "dark");
+});
+toggle.addEventListener("click", () => toggle.classList.toggle("active"));
 
